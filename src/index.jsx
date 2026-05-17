0a304cd7c0ed47f32707803b218ae701c12266a8
Daniel Somtoochi Dav-Emmanuel <74049793+royalpriniel@users.noreply.github.com>
2026-05-17 18:10:19 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client';
-import {LoveCalculator} from './loveCalculator';
+import LoveCalculator from './loveCalculator';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<LoveCalculator />);

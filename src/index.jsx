298c5464440f377bf3914a3d56cc92849939e141
Daniel Dav-Emmanuel <royalpriniel@gmail.com>
2026-05-17 16:10:22 +0100

    Add multiple folders from phone

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import {createRoot} from 'react-dom/client';
+import {LoveCalculator} from './loveCalculator';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<LoveCalculator />);
